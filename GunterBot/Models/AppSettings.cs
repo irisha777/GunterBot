91dbf5cbf2db0f85d9d13881487aef01283597ff
Rishat <rishatgabdullin1994@gmail.com>
2020-10-21 12:30:13 +0300

    asd

--- GunterBot/Models/AppSettings.cs
+++ GunterBot/Models/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://e166db5647ac.ngrok.io";
+        public static string Url { get; set; } = "https://b8cdfc4200e9.ngrok.io";
         public static string Name { get; set; } = "ShitBot";
         public static string Key { get; set; } = "1264975729:AAETMHOibYm9qS4untjUcw1iVwcTwuS970M";
     }
