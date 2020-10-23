b99e4dfe3b8a0b9ae9fde22709313c08c06bcb28
Rishat <rishatgabdullin1994@gmail.com>
2020-10-23 10:50:14 +0300

    fwefwefw

--- GunterBot/Models/AppSettings.cs
+++ GunterBot/Models/AppSettings.cs
@@ -4,7 +4,7 @@ namespace ShitBot.Models
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://b9ad3a5d305c.ngrok.io";
+        public static string Url { get; set; } = "https://895f3cefd33a.ngrok.io";
         public static string Name { get; set; } = "ShitBot";
         public static string Key { get; set; } = "1264975729:AAETMHOibYm9qS4untjUcw1iVwcTwuS970M";
         public static string RatesKey { get; set; } = "a9ce9c684b1041078a05a9e04d38ec3f";
